37c0608363cda554dd64f267c0349cebc7fdb1d4
carlaylett <carl@labworks.io>
2023-10-09 23:56:09 +0100

    tweaks

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const adminChargeRate = parseFloat(document.getElementById("admin-charge-rate").value) || 0.29588;
 
         const unitRateBillAmount = (currentReading - lastReading) * unitRate;
-        unitRateBillAmountSpan.textContent = `$${unitRateBillAmount.toFixed(2)}`;
+        unitRateBillAmountSpan.textContent = `£${unitRateBillAmount.toFixed(2)}`;
 
         if (lastBillDate && adminChargeRate) {
             const lastBillDateObj = new Date(lastBillDate);
             const currentDate = new Date();
             const daysDifference = Math.floor((currentDate - lastBillDateObj) / (1000 * 60 * 60 * 24));
             const adminChargeSoFar = daysDifference * adminChargeRate;
-            adminChargeSoFarSpan.textContent = `$${adminChargeSoFar.toFixed(2)}`;
+            adminChargeSoFarSpan.textContent = `£${adminChargeSoFar.toFixed(2)}`;
 
             if (!isNaN(unitRateBillAmount)) {
                 const totalBill = unitRateBillAmount + adminChargeSoFar;
-                totalBillSpan.textContent = `$${totalBill.toFixed(2)}`;
+                totalBillSpan.textContent = `£${totalBill.toFixed(2)}`;
             } else {
                 totalBillSpan.textContent = "";
             }
